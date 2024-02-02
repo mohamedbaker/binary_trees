7598ac65f58a8c9727ecd1648b79b3a0bc14a445
mohamedbaker <dam.vampire22@gmail.com>
2024-02-02 04:07:52 +0200

    heghit

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures binary tree height.
+ * @tree: the tree .
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_levels = 0;
+	size_t right_levels = 0;
+
+	if (!tree)
+		return (0);
+
+	left_levels = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_levels = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left_levels > height_r ? left_levels : right_levels);
+}
