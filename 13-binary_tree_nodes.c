4ba9afca681fc631d71ad375ec70ae3738968ab5
mohamedbaker <dam.vampire22@gmail.com>
2024-02-02 04:25:31 +0200

    a

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,5 +10,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (tree->left || tree->right) + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	return ((tree->left || tree->right)
+			+ binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
